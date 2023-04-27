50e4ac20a1d2fa62dfdecf023e8fdce1bdf46608
Parris Koumouris <parris.koumouris@acl.org.au>
2023-04-27 16:47:25 +1000

    Production db deets

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -7,9 +7,8 @@ class GeneralController < ApplicationController
 
     def gnaf
         response = HTTParty.get("https://api.psma.com.au/v2/addresses/geocoder?additionalProperties=localGovernmentArea,stateElectorate,commonwealthElectorate,asgsMain&address=#{params[:address]}",
-            :headers => {"Authorization"=>ENV['GNAF_API_KEY']})
-        render json: {
-            response: response.body
-        }.to_json
+            :headers => {"Authorization"=>'0YONURTjk2DbMU4zKFViQb8MuqAPTOoZ'})
+        puts response
+        render json: response.to_json
     end
 end
