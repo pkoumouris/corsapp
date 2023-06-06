c0dfe5e87af6bd387af78852cfe4e59b454f7b6a
Parris Koumouris <parris.koumouris@acl.org.au>
2023-06-07 08:38:03 +1000

    Handle bad address

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -93,8 +93,12 @@ class GeneralController < ApplicationController
         wem_failure = false # will change as time goes on
         nb_failure = false # will change as time goes on
         # 3a. Make the calls to NB
-        nb_resp = donation.create_in_nationbuilder_with_address
-        puts nb_resp
+        if donation.gnaf_address_identifier.nil? || donation.gnaf_address_identifier.length < 3
+            nb_resp = donation.create_in_nationbuilder
+        else
+            nb_resp = donation.create_in_nationbuilder_with_address
+        end
+        #puts nb_resp
         nb_failure = (nb_resp.code != 201)
         if !nb_failure
             donation.update_attribute(:nbid, nb_resp['data']['id'])
@@ -184,7 +188,11 @@ class GeneralController < ApplicationController
         wem_failure = false # will change as time goes on
         nb_failure = false # will change as time goes on
         # 3a. Make the calls to NB
-        nb_resp = donation.create_in_nationbuilder_with_address
+        if donation.gnaf_address_identifier.nil? || donation.gnaf_address_identifier.length < 3
+            nb_resp = donation.create_in_nationbuilder
+        else
+            nb_resp = donation.create_in_nationbuilder_with_address
+        end
         puts nb_resp
         nb_failure = (nb_resp.code != 201)
         if !nb_failure
